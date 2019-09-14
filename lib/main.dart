d54da59caf2d37ff0673e60636ddc5b330a4738c
hisnameispum <sirasitxp@gmail.com>
2019-09-14 16:28:14 -0700

    Added Correct, and Wrong Icons

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,16 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -86,7 +96,7 @@ class _QuizPageState extends State<QuizPage> {
         ),
         //TODO: Add a Row here as your score keeper
         Row(
-          children: <Widget>[],
+          children: <Widget>[scoreKeeper[0], scoreKeeper[1]],
         )
       ],
     );
