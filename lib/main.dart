b5bd26f704a09186f07f8b0d4965b115373f853f
hisnameispum <sirasitxp@gmail.com>
2019-09-16 15:26:39 -0700

    Added a list of questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(Quizzler());
 
@@ -26,6 +27,11 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[Random().nextInt(3)],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
